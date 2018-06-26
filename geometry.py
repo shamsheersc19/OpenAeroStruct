fix in geometry.py for correct cs gradients for streamwise_chords

geometry.py
@@ -420,9 +420,6 @@ class GeometryMesh(Component):
 
         self.add_output('mesh', val=self.mesh)
 
-        # If the user doesn't provide the radius or it's not a desver, then we must
-        # compute it here.
-
         self.add_output('chords_fem', val=np.ones((ny - 1)))
         self.add_output('streamwise_chords', val=np.ones((ny - 1)))
         self.add_output('twist_fem', val=np.ones((ny - 1)))
@@ -452,7 +449,7 @@ class GeometryMesh(Component):
         rotate(mesh, self.geo_params['twist'], self.symmetry, self.rotate_x)
 
         ch_fem = chords_fem(mesh)
-        unknowns['streamwise_chords'] = ch_fem
+        unknowns['streamwise_chords'] = ch_fem.copy()
         twist_fem = ch_fem.copy()
         
         surface = self.surface